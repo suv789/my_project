25c2ec83b7c8e8ff39796972b37cf767aec43325
suv789 <suvajitbera789@gmail.com>
2023-09-26 22:32:49 +0530

    Add all the code

--- text_to_voice.py
+++ text_to_voice.py
@@ -1,1 +1,15 @@
-print("hello world!")
+import pyttsx3
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voices',voices[0].id)
+engine.setProperty('rate',150)
+
+while True:
+    text = input("Enter what you want to say (or type exit to quit): ")
+    if text.lower()=='exit':
+        break
+
+    engine.say(text)
+    engine.runAndWait()
